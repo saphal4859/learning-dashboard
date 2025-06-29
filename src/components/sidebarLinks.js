52c01dc020418abf604132ff851cc53fdbcc2dcb
saphal4859 <saisaphal123@gmail.com>
2025-06-29 15:55:59 +0530

    Updated react

--- src/components/sidebarLinks.js
+++ src/components/sidebarLinks.js
@@ -7,24 +7,71 @@ const sidebarLinks = [
     items: [
       {
         name: "Basics",  // ✅ changed from 'label'
-        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-533/ca07030e2429cf0"  // ✅ full ClickUp URL
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-1293/1638d0cccf4dd36/2ky42j75-1313"  // ✅ full ClickUp URL
       },
       {
         name: "Default Authentication",
-        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-553/0a739ac5293d50c"
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-1413/bf34b33011c74e3"
       },
       {
         name: "Basic Authentication",
-        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-593/6b62f047d3d0067"
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-1473/35adfe260f650a1"
       },
       {
         name: "In memory and JDBC Authentication",
-        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-613/1cd0b851d1eaf05"
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-1533/4c63f64f2e02d74"
       },
       {
         name: "Hashing and Password Encoder",
-        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-633/ec4b1ec9b2e8ca1"
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-1593/52fe5e94ab8ed55"
+      },
+      {
+        name: "JWT",
+        url : "https://doc.clickup.com/90131482853/d/h/2ky42j75-1653/bf075c03090e2e9"
       }
+      ,
+      {
+        name: "Authentication Entry Point",
+        url : "https://doc.clickup.com/90131482853/d/h/2ky42j75-1713/80360f65c58b120"
+      }
+      ,
+      {
+        name: "JWT flow",
+        url : "https://doc.clickup.com/90131482853/d/h/2ky42j75-1753/1193df102c55a40"
+      }
+    ]
+  },
+  {
+    category: "React",
+    icon: "⚛️",
+    items: [
+      {
+        name: "Basics",
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-873/87818e9724a0153"
+      },
+      {
+        name: "Props",
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-913/ae4a6cfe95441d9"
+      }
+      ,
+      {
+        name: "Hooks",
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-953/0f33cd1507d319f"
+      }
+      ,
+      {
+        name: "Api",
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-1053/dc55ae9a2b83cd1"
+      }
+      ,
+      {
+        name: "Interceptors",
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-1073/744a29c4d84bf9a"
+      },
+      {
+        name : "Forms",
+        url : "https://doc.clickup.com/90131482853/d/h/2ky42j75-1093/ce92b6ed5bac4eb"
+      },
     ]
   },
   {
@@ -33,8 +80,9 @@ const sidebarLinks = [
     items: [
       {
         name: "Overview",
-        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-653/microservice-overview"
-      }
+        url: "https://doc.clickup.com/90131482853/d/h/2ky42j75-1293/1638d0cccf4dd36"
+      },
+      
     ]
   }
 ];
